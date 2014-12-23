6e371f39b09c2fd91e34c998ae3e0a19075ad7b4
davidtangness <erictangness@yahoo.ca>
2014-12-22 20:11:16 -0500

    Finished Tutorial

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,6 +5,21 @@ public class DestroyByContact : MonoBehaviour {
 
 	public GameObject explosion;
 	public GameObject shipExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+
+	void Start(){
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent <GameController> ();
+		}
+		if (gameController == null){
+			Debug.Log("Cannot find 'GameController' script");
+		}
+	}
+
+
 
 	void OnTriggerEnter(Collider other){
 
@@ -12,12 +27,13 @@ public class DestroyByContact : MonoBehaviour {
 			return;
 		}
 
-
 		Instantiate (explosion, transform.position, transform.rotation);
 
 		if (other.tag == "Player") {
 			Instantiate (shipExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
 		}
+		gameController.AddScore (scoreValue);
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,11 +10,33 @@ public class GameController : MonoBehaviour {
 	public float startWait;
 	public float waveWait;
 
+	public UnityEngine.UI.Text scoreText;
+	public UnityEngine.UI.Text gameOverText;
+	public UnityEngine.UI.Text restartText;
+
+	private int score;
+	private bool gameOver;
+	private bool restart;
+
 	// Use this for initialization
 	void Start () {
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
 		StartCoroutine(SpawnWaves());
 	}
-	
+
+	void Update(){
+		if (restart){
+			if(Input.GetKeyDown (KeyCode.R)){
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
+
 	IEnumerator SpawnWaves () {
 
 		yield return new WaitForSeconds(startWait);
@@ -27,6 +49,27 @@ public class GameController : MonoBehaviour {
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds(waveWait);
+
+			if (gameOver){
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
 		}
 	}
+
+	public void AddScore(int newScoreValue){
+			score += newScoreValue;
+			UpdateScore ();
+	}
+
+	void UpdateScore(){
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver(){
+		gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+
 }
